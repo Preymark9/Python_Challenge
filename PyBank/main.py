14bdba3167d1c40fd243906db1d257827f0d0c7d
major <majora1930@gmail.com>
2018-07-17 18:10:50 -0500

    I added my comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,4 +3,7 @@ import csv
 
 csvpath = r"C:\Users\major\OneDrive\Documents\WASHSTL201806DATA4-Class-Repository-DATA\Week 3 - Python\Homework\PyBank\Resources\budget_data.csv"
 with open(csvpath, "r") as csvfile:
-    
+    #find the highest value on the revenue column
+    #also find the lowest value on the revenue column
+    #what was the biggest growth in a month?
+    #what the biggest loss in a month?
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,4 +3,7 @@ import csv
 
 csvpath = r"C:\Users\major\OneDrive\Documents\WASHSTL201806DATA4-Class-Repository-DATA\Week 3 - Python\Homework\PyPoll\Resources\election_data.csv"
 with open(csvpath, "r") as csvfile:
-    
+    #who are the candidates
+    #who got the most votes
+    #who got the least votes
+    #tally the votes for everyone
