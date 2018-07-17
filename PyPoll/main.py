5038c5b59932816be454d4bb272d378a551c64d9
major <majora1930@gmail.com>
2018-07-17 18:05:03 -0500

    Hope this works

---
+++ PyPoll/main.py
@@ -0,0 +1,6 @@
+import os
+import csv
+
+csvpath = r"C:\Users\major\OneDrive\Documents\WASHSTL201806DATA4-Class-Repository-DATA\Week 3 - Python\Homework\PyPoll\Resources\election_data.csv"
+with open(csvpath, "r") as csvfile:
+    
